b824dc5440d6d02e2ad23d2e05e9841a7a05a045
JuanCamilo9915 <jcamiloduque0215@gmail.com>
2024-01-08 21:37:32 -0500

    Se solucionan los detalles de duplicidad en el último menú hijo qe presentaba el código en la sección del armado del menú y se deja funcionando al 100% x 100%. Cabe mencionar que esa lógica debe refactorizarce porque hacosas que pueden llegar a ser confusas en el mismo.

--- src/services/Logica.js
+++ src/services/Logica.js
@@ -1,3 +1,13 @@
+/* Nota:
+este código será refactorizado, esto debido a la manipulación de algunos objetos y areglos
+los cuales seguir el flujo de esa lógica es un poco complejo y confuso apesar de que
+funciona perfectamente.
+
+Cabe mencionar que esta lógica se dejara comentada com recordatorio y aprendizaje.
+*/
+/**
+ * @param { list, menu } res 
+ */
 export function mapStructureMenu(res) {
     const { list, menu } = res;
 
@@ -13,144 +23,153 @@ export function mapStructureMenu(res) {
         })[menu.level](menu.id);
     });
 
-    // findChildrenForParent(menu);
     mapMenuOfUser({ grandFather, parent, page, listUsers: list });
 }
 
+/**
+ * 
+ * @param { grandFather, parent, page, listUsers } menu 
+ */
 function mapMenuOfUser(menu) {
     const { grandFather, parent, page, listUsers } = menu;
     let idMenuToSearch = undefined;
     let isGrandFather = undefined;
     let existsProperty = undefined;
     let itemMenu = undefined;
-    const menuOk = [];
+    let menuOk = [];
+    // let itemsChildren = [];
+    // let resourcesChildren = [];
 
-    // const addChildToParent = (isMenuRoot, pageChild) => {
-    //     // debugger;
-    //     itemMenu = isMenuRoot ? grandFather : parent;
+    const addChildToParent = (isMenuRoot, pageChild) => {
+        itemMenu = isMenuRoot ? grandFather : parent;
 
-    //     existsProperty = itemMenu[idMenuToSearch].hasOwnProperty('children');
+        existsProperty = itemMenu[idMenuToSearch].hasOwnProperty('children');
 
-    //     if (!existsProperty) itemMenu[idMenuToSearch].children = [];
+        if (!existsProperty) itemMenu[idMenuToSearch].children = [];
 
-    //     // validar el type para saber si se busca en los siguientes children o no
-    //     // validar el type si es 2 agregar si es 1 buscar y si es cero buscar
-    //     // console.log('itenMenu: ', itemMenu);
-    //     // console.log('pageChild: ', pageChild);
-
-    //     itemMenu[idMenuToSearch].children.push(pageChild);
-
-    //     if (isMenuRoot) return;
-
-    //     idMenuToSearch = itemMenu[idMenuToSearch].parentItemId;
-    //     isGrandFather = grandFather[idMenuToSearch] !== undefined;
-    //     addChildToParent(isGrandFather, itemMenu);
-    // }
-
-    // const addChildToParent2 = (isMenuRoot, pageChild) => {
-    //     if (isMenuRoot) {
-    //         existsProperty = grandFather[idMenuToSearch].hasOwnProperty('children');
-
-    //         if (!existsProperty) grandFather[idMenuToSearch].children = [];
-    //         grandFather[idMenuToSearch].children.push(pageChild);
-    //     } else {
-    //         existsProperty = parent[idMenuToSearch].hasOwnProperty('children');
-
-    //         if (!existsProperty) parent[idMenuToSearch].children = [];
-    //         parent[idMenuToSearch].children.push(pageChild);
-
-    //         idMenuToSearch = parent[idMenuToSearch].parentItemId;
-    //         isGrandFather = grandFather[idMenuToSearch] !== undefined;
-    //         addChildToParent2(isGrandFather, parent);
-    //     }
-    // }
-
-    const addChildToParent = (items, resources) => {
-        if (items.length === 1) {
-            const x = Object.values(page)
-                .filter(item => item.parentItemId === items[0].id);
-                console.log('children of item: ', x);
-            items[0].children = x;
-            grandFather[idMenuToSearch].children.push(...items);
-            grandFather[idMenuToSearch].children.push(...resources);
+        if (itemMenu[idMenuToSearch].level === 0) {
+            itemMenu[idMenuToSearch].children.push(pageChild);
             return;
         }
 
-        // for (const i of resources) {
-            
-        // }
-        for (const i of page) {
-            parent[page[i].parentItemId].children = 
-            parent[page[i].parentItemId].children.push();
+        if (!isMenuRoot) {
+            Object.values(page)
+            .filter(item => item.parentItemId === idMenuToSearch)
+            .forEach(item => {
+                itemMenu[idMenuToSearch].children.push(item)
+                delete page[item.id];
+            });
         }
+
+        idMenuToSearch = itemMenu[idMenuToSearch].parentItemId;
+        isGrandFather = grandFather[idMenuToSearch] !== undefined;
+        addChildToParent(isGrandFather, ...Object.values(itemMenu));
     }
 
     const mapOutMenu = () => {
-        let itemsChildren = [];
-        let resourcesChildren = [];
+        for (const keyPage in page) {
+            idMenuToSearch = page[keyPage].parentItemId;
 
-        for (const keyIdMenu in grandFather) {
-            itemsChildren = Object.values(parent)
-                .filter(item => item.parentItemId === +keyIdMenu);
-
-            resourcesChildren = Object.values(page)
-                .filter(item => item.parentItemId === +keyIdMenu);
-
-            if (itemsChildren.length === 0) {
-                grandFather[keyIdMenu].children = resourcesChildren;
-            } else {
-                // itemsChildren.push([...resourcesChildren]);
-                console.log('items: ', itemsChildren);
-                console.log('resource: ', resourcesChildren);
-                grandFather[keyIdMenu].children = [];
-                idMenuToSearch = keyIdMenu;
-                addChildToParent(itemsChildren, resourcesChildren);
+            if (grandFather[idMenuToSearch]) {
+                isGrandFather = true;
+                addChildToParent(isGrandFather, page[keyPage]);
+            }
+            else {
+                isGrandFather = false;
+                addChildToParent(isGrandFather, page[keyPage]);
             }
         }
-        console.log(itemsChildren);
-        console.log(resourcesChildren);
     }
-    // const mapOutMenu = () => {
-    //     let x = [];
-    //     for (const keyPage in page) {
-    //         idMenuToSearch = page[keyPage].parentItemId;
-
-    //         if (grandFather[idMenuToSearch]) {
-    //             isGrandFather = true;
-    //             addChildToParent(isGrandFather, page[keyPage]);
-    //         }
-    //         else {
-    //             isGrandFather = false;
-    //             addChildToParent(isGrandFather, page[keyPage]);
-    //         }
-    //     }
-    // }
 
     listUsers.forEach(user => {
-        if (user.id === 1) {
-            mapOutMenu();
-            console.log(JSON.stringify(grandFather, null, 2));
-            return;
-        }
+        console.log(`\n\n-------------User: ${user.name}-------------`);
+        mapOutMenu();
+        menuOk = Object.values(grandFather);
+        console.log('Menu user # 1: ', JSON.stringify(grandFather, null, 2));
+        console.log('\n\n\nMenu user # 1.2: ', JSON.stringify(menuOk, null, 2));
     });
 }
 
-function findChildrenForParent(menu) {
-    const isChildItem = (isChild) => isChild.type === 1 && isChild.parentItemId === isChild.id;
+// Nota: este código esta en desarrollo y es la mejora que se va a realizar al código anterior.
+// const addChildToParent = (isMenuRoot, pageChild) => {
+//     // debugger;
+//     itemMenu = isMenuRoot ? grandFather : parent;
 
-    const zero = menu.filter(item => item.type === 0);
-    const one = menu.filter(item => item.type === 1);
-    const two = menu.filter(item => item.type === 2);
+//     existsProperty = itemMenu[idMenuToSearch].hasOwnProperty('children');
 
-    for (const item of zero) {
-        console.log(item);
-    }
-}
+//     if (!existsProperty) itemMenu[idMenuToSearch].children = [];
 
-// function findChildrenForParent(menu) {
-//     Object.assign(destino, agregar);
+//     // validar el type para saber si se busca en los siguientes children o no
+//     // validar el type si es 2 agregar si es 1 buscar y si es cero buscar
+//     // console.log('itenMenu: ', itemMenu);
+//     // console.log('pageChild: ', pageChild);
+
+//     itemMenu[idMenuToSearch].children.push(pageChild);
+
+//     if (isMenuRoot) return;
+
+//     idMenuToSearch = itemMenu[idMenuToSearch].parentItemId;
+//     isGrandFather = grandFather[idMenuToSearch] !== undefined;
+//     addChildToParent(isGrandFather, itemMenu);
 // }
 
-// function findChildrenForParent(menu) {
-// buscar por padre y aplicar filter para saber que hijs tiene ese padre
+// const mapOutMenu = () => {
+//     for (const keyIdMenu in grandFather) {
+//         itemsChildren = Object.values(parent)
+//             .filter(item => item.parentItemId === +keyIdMenu);
+
+//         resourcesChildren = Object.values(page)
+//             .filter(item => item.parentItemId === +keyIdMenu);
+
+//         if (itemsChildren.length === 0) {
+//             grandFather[keyIdMenu].children = resourcesChildren;
+//         } else {
+//             // itemsChildren.push([...resourcesChildren]);
+//             // console.log('items: ', itemsChildren);
+//             // console.log('resource: ', resourcesChildren);
+//             grandFather[keyIdMenu].children = [];
+//             idMenuToSearch = keyIdMenu;
+//             addChildToParent(itemsChildren, resourcesChildren);
+//         }
+//     }
+//     // console.log(itemsChildren);
+//     // console.log(resourcesChildren);
+// }
+
+// const addChildToParent2 = (isMenuRoot, pageChild) => {
+//     if (isMenuRoot) {
+//         existsProperty = grandFather[idMenuToSearch].hasOwnProperty('children');
+
+//         if (!existsProperty) grandFather[idMenuToSearch].children = [];
+//         grandFather[idMenuToSearch].children.push(pageChild);
+//     } else {
+//         existsProperty = parent[idMenuToSearch].hasOwnProperty('children');
+
+//         if (!existsProperty) parent[idMenuToSearch].children = [];
+//         parent[idMenuToSearch].children.push(pageChild);
+
+//         idMenuToSearch = parent[idMenuToSearch].parentItemId;
+//         isGrandFather = grandFather[idMenuToSearch] !== undefined;
+//         addChildToParent2(isGrandFather, parent);
+//     }
+// }
+
+// const addChildToParent = (items, resources) => {
+//     if (items.length === 1) {
+//         const x = Object.values(page)
+//             .filter(item => item.parentItemId === items[0].id);
+//             console.log('children of item: ', x);
+//         items[0].children = x;
+//         grandFather[idMenuToSearch].children.push(...items);
+//         grandFather[idMenuToSearch].children.push(...resources);
+//         return;
+//     }
+
+//     // for (const i of resources) {
+
+//     // }
+//     for (const i of page) {
+//         parent[page[i].parentItemId].children =
+//         parent[page[i].parentItemId].children.push();
+//     }
 // }
